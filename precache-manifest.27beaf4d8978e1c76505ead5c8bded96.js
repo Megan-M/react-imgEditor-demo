00fb0374399292c57f46c745d4fa42a87d8dcb13
MM <meevelm@gmail.com>
2019-05-12 23:14:27 -0400

    Updates

---
+++ precache-manifest.27beaf4d8978e1c76505ead5c8bded96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebed185b275252cb25a6b042da13427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f75309221518968c3e",
+    "url": "/static/css/main.42460829.chunk.css"
+  },
+  {
+    "revision": "f5d7671e14b7abb18a1c",
+    "url": "/static/js/2.af8e8dba.chunk.js"
+  },
+  {
+    "revision": "35f75309221518968c3e",
+    "url": "/static/js/main.e58ddbce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
